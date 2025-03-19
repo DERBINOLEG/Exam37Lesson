b2ad85d7d0906af3f0236cd97677ef564ede481a
Олег Дербин <Olegmanchik@gmail.com>
2025-03-19 22:08:48 +0300

    add models

---
+++ Exam37Lesson/Model/CharacterModel.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterModel.swift
+//  Exam37Lesson
+//
+//  Created by Олег Дербин on 19.03.2025.
+//
+
+import Foundation
+
+struct CharacterModel {
+    let imageName: String
+    let description: String
+}
---
+++ Exam37Lesson/Model/CustomViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CustomViewModel.swift
+//  Exam37Lesson
+//
+//  Created by Олег Дербин on 19.03.2025.
+//
+
+import Foundation
+
+struct CustomViewModel {
+    let sectionTitle: String
+    let characterInfo: [CharacterModel]
+}
